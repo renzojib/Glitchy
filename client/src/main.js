dc367b4b2a4fcb1e9210783815c351383edb9e5b
renzojib <105753623+renzojib@users.noreply.github.com>
2022-12-22 00:28:14 -0500

    audit

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { loadFonts } from './plugins/webfontloader'
-loadFonts()
 
 createApp(App)
   .use(router)
