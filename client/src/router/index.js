b27659538601b6b10ddb63b04f03b7d14c2ac587
renzojib <105753623+renzojib@users.noreply.github.com>
2022-12-27 23:29:11 -0500

    change to hash history

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MovieView from '../views/MovieView.vue'
 
@@ -20,7 +20,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
